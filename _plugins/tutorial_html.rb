e617f2b37d85440a9c459bbea2e1492b1bcfca8a
Hugo Ruscitti <hugoruscitti@gmail.com>
2020-05-03 18:19:36 -0300

    Creando componente para añadir tutoriales en formato HTML

---
+++ _plugins/tutorial_html.rb
@@ -0,0 +1,31 @@
+require "jekyll-spark"
+
+module Jekyll
+  class TutorialHTMLComponent < ComponentTag
+    def template(context)
+      titulo = @props["titulo"]
+      nombre = @props["nombre"]
+      autor = @props["autor"]
+      descripcion = @props["descripcion"]
+
+      render = %Q[
+        <div class="flex pb3">
+          <div class="ph2">
+            <a href="/tutoriales/#{nombre}/"><img src="/tutoriales/#{nombre}/icono.png"></a>
+          </div>
+
+          <div>
+            <a class='link' href="/tutoriales/#{nombre}/">#{titulo}</a>
+            <div class="pv2">#{descripcion}</div>
+            <div>Realizado por: <strong>#{autor}</strong></div>
+          </div>
+        </div>
+      ]
+    end
+  end
+end
+
+Liquid::Template.register_tag(
+  "tutorial_html",
+  Jekyll::TutorialHTMLComponent,
+)
